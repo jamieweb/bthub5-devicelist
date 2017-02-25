e348a3d74c960f12f965f906e3ed8c812db66a55
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-02-25 02:27:30 +0000

    Fixed poor regex

--- device-list.sh
+++ device-list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-devices=($(curl -s 192.168.1.254 | tr "<" "\n" | grep "TD class=\"bt_border\" align=LEFT valign=MIDDLE width=\"2[35]%\">" | sed -e "s/^TD class=\"bt_border\" align=LEFT valign=MIDDLE width=\"2[35]%\">//; s/&nbsp;//; s/2.4 GHz Wireless:/2.4 GHz Wireless:\n/; s/5 GHz Wireless:/\n5 GHz Wireless:\n/; s/Ethernet:/Ethernet:\n/; s/USB:/\nUSB:\n/" | grep -B 1 -A 1 [0123456789abcdef][0123456789abcdef]:[0123456789abcdef][0123456789abcdef] | tr '\n' ' ' | sed -e s/" -- "/"\n"/g | tr ' ' '_'))
+devices=($(curl -s 192.168.1.254 | tr "<" "\n" | grep "TD class=\"bt_border\" align=LEFT valign=MIDDLE width=\"2[35]%\">" | sed -e "s/^TD class=\"bt_border\" align=LEFT valign=MIDDLE width=\"2[35]%\">//; s/&nbsp;//; s/2.4 GHz Wireless:/2.4 GHz Wireless:\n/; s/5 GHz Wireless:/\n5 GHz Wireless:\n/; s/Ethernet:/Ethernet:\n/; s/USB:/\nUSB:\n/" | grep -B 1 -A 1 [0-9a-f][0-9a-f]:[0-9a-f][0-9a-f] | tr '\n' ' ' | sed -e s/" -- "/"\n"/g | tr ' ' '_'))
 
 mapfile -t known < known-devices.txt
 
